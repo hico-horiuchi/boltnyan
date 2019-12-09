30f6a77cbe36c9f0db106430fad857600e1392a4
Akihiko Horiuchi <12ff5b8@gmail.com>
2019-12-09 12:12:35 +0900

    try message menu

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const app = new App({
   logLevel: LogLevel.DEBUG
 });
 
-app.message('hello', ({ message, say }) => {
+app.message('boltnyan hello', ({ message, say }) => {
   say(`Hey there <@${message.user}>!`);
 });
 
 // https://api.slack.com/docs/message-buttons
-app.message('game', ({ message, say }) => {
+app.message('boltnyan button', ({ message, say }) => {
   say({
     text: 'Would you like to play a game?',
     attachments: [{
       text: 'Choose a game to play',
       fallback: 'You are unable to choose a game',
-      callback_id: 'wopr_game',
+      callback_id: 'game_button',
       color: '#3AA3E3',
       attachment_type: 'default',
       actions: [{
@@ -47,9 +47,56 @@ app.message('game', ({ message, say }) => {
   });
 });
 
-app.action({ callback_id: 'wopr_game' }, ({ body, ack, say }) => {
+app.action({ callback_id: 'game_button' }, ({ body, ack, say }) => {
   ack();
   say(`<@${body.user.id}> clicked ${body.actions[0].value}`);
+});
+
+// https://api.slack.com/docs/message-menus
+app.message('boltnyan menu', ({ message, say }) => {
+  say({
+    text: 'Would you like to play a game?',
+    response_type: 'in_channel',
+    attachments: [{
+      text: 'Choose a game to play',
+      fallback: "If you could read this message, you'd be choosing something fun to do right now.",
+      color: '#3AA3E3',
+      attachment_type: 'default',
+      callback_id: 'game_menu',
+      actions: [{
+        name: 'games_list',
+        text: 'Pick a game...',
+        type: 'select',
+        options: [{
+          text: 'Hearts',
+          value: 'hearts'
+        }, {
+          text: 'Bridge',
+          value: 'bridge'
+        }, {
+          text: 'Checkers',
+          value: 'checkers'
+        }, {
+          text: 'Chess',
+          value: 'chess'
+        }, {
+          text: 'Poker',
+          value: 'poker'
+        }, {
+          text: "Falken's Maze",
+          value: 'maze'
+        }, {
+          text: 'Global Thermonuclear War',
+          value: 'war'
+        }]
+      }]
+    }]
+  });
+});
+
+app.action({ callback_id: 'game_menu' }, ({ body, ack, say }) => {
+  ack();
+  say(`<@${body.user.id}> selected ${body.actions[0].selected_options[0].value}`);
 });
 
 app.error((error) => {
